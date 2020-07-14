eec6b40d82c05bc1d767b8943934d33db78b4851
Julius Lehmpuhl <julius.lehmpuhl@gmail.com>
2020-07-14 21:18:47 +0200

    first bar plot

---
+++ DataVisualisation.py
@@ -0,0 +1,70 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import cx_Oracle
+import sys
+import sql_dataset_creation as dc
+
+def VisualizeClickRank():
+    """Visualizes the clicks base on the rang in a bar diagram"""
+    with connection.cursor() as dbcursor:
+        dbcursor.execute("""
+            SELECT *
+            FROM RANK_FLU
+        """)
+        flu_ranks = dbcursor.fetchall()
+        #remove null 
+        flu_ranks = [(0, x[1]) if x[0]==None else x for x in flu_ranks]
+        flu_ranks.sort(key=lambda tup: tup[0])
+
+        dbcursor.execute("""
+            SELECT *
+            FROM RANK_SYMPTOMS
+        """)
+
+        symptomes_ranks = dbcursor.fetchall()
+        #remove null 
+        symptomes_ranks = [(0, x[1]) if x[0]==None else x for x in symptomes_ranks]
+        symptomes_ranks.sort(key=lambda tup: tup[0])
+
+        #Draw data
+        sns.set(style="white", context="talk")
+        f, (ax1, ax2) = plt.subplots(2, 1, figsize=(7, 5))
+
+        ranks, count = zip(*flu_ranks)
+        x1 = np.array(ranks[:10])
+        sns.barplot(x=x1, y=count[:10], palette="deep", ax=ax1).set_title("Suchen nach \"flu\" abhängig vom Rang")
+        ax1.axhline(0, color="k", clip_on=False)
+        ax1.set_ylabel("Clicks")
+        ax1.set_xlabel("Rang")
+
+
+        ranks, count = zip(*symptomes_ranks)
+        x2 = np.array(ranks[:10])
+        sns.barplot(x=x2, y=count[:10], palette="deep", ax=ax2).set_title("Suchen nach \"symptom\" abhängig vom Rang")
+        ax2.axhline(0, color="k", clip_on=False)
+        ax2.set_ylabel("Clicks")
+        ax2.set_xlabel("Rang")
+
+        # Finalize the plot
+        sns.despine(bottom=False)
+        #plt.setp(f.axes, yticks=[0, 500, 1000, 1500, 2000, 2500])
+        plt.tight_layout(h_pad=2)
+        plt.show()
+
+    
+try:
+    cx_Oracle.init_oracle_client(lib_dir=r"./instantclient")
+except Exception as err:
+    print("Fehler beim initialisieren des Oracle Clients.")
+    print(err)
+    sys.exit(1)
+
+
+username = ""
+password = ""
+
+connection = cx_Oracle.connect(username, password, "localhost/rispdb1")
+
+VisualizeClickRank()
